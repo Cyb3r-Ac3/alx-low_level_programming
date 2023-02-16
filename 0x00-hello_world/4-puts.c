ebe85e8b0bdf823e1b441a29a3d87ebf1e34ae8d
Anthony Odey <anthonyogar61@gmail.com>
2023-02-16 10:05:04 -0800

    added new line

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main - a c languange that prints a line with the puts function
+ * main - A program that prints a line with the puts function
  * Return: 0 (Once the program returns 0 meaning it ran succesfully)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzule\n");
+	puts("\"Programming is like building a multilingual puzzule");
 	return (0);
 }
 
