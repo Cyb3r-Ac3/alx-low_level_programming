5d2727547fc0c0dcf2589c7e92c9ac567cc73903
Anthony Odey <anthonyogar61@gmail.com>
2023-02-16 19:39:28 -0800

    Quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - A c program that prints a line to the  standard error
+ * Return: 1 (Succes)
+ */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora korpar, 2015-10-19,\n" stdout);
+	return (1);
