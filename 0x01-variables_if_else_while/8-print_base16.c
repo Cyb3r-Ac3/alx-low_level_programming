992c7cc0a8a142135eca5cffcf94f726062010bb
Anthony Odey <anthonyogar61@gmail.com>
2023-02-17 15:02:34 -0800

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - prints all the numbers of baase 16 in lowercase
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int d;
+
+	char c;
+
+	c = 'a';
+	d = 0;
+	while
+		(d < 10) {
+			putchar(d + '0');
+			d++;
+		}
+	while
+		(c <= 'f') {
+			putchar(c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
