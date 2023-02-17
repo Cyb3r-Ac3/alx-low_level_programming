d13cf13940d7203996d727598f91de5379c359de
Anthony Odey <anthonyogar61@gmail.com>
2023-02-16 20:19:23 -0800

    fixed the Error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,5 +6,5 @@
  */
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora korpar, 2015-10-19\n", stdout);
+	fputs("and that piece of art is useful\" - Dora korpar, 2015-10-19\n", stdout);
 	return (1);
