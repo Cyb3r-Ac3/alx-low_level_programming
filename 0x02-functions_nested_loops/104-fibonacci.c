0f874b1789a07e2bb009bccdecf34068541fda82
Anthony Odey <anthonyogar61@gmail.com>
2023-02-21 21:10:06 -0800

    fixed error

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -6,45 +6,46 @@
  *
  * Return: Always 0
  */
+
 int main(void)
 {
-	int cnt;
-	unsigned long fib8 = 0, fib9 = 1, sum;
-	unsigned long fib8_half1, fib8_half2, fib9_half1, fib9_half2;
+	int count;
+	unsigned long fib1 = 0, fib2 = 1, sum;
+	unsigned long fib1_half1, fib1_half2, fib2_half1, fib2_half2;
 	unsigned long half1, half2;
 
-	for (cnt = 0; cnt < 92; cnt++)
+	for (count = 0; count < 92; count++)
 	{
-		sum = fib8 + fib9;
+		sum = fib1 + fib2;
 		printf("%lu, ", sum);
 
-		fib8 = fib9;
-		fib9 = sum;
+		fib1 = fib2;
+		fib2 = sum;
 	}
 
-	fib8_half1 = fib8 / 10000000000;
-	fib9_half1 = fib9 / 10000000000;
-	fib8_half2 = fib8 / 10000000000;
-	fib9_half2 = fib9 / 10000000000;
+	fib1_half1 = fib1 / 10000000000;
+	fib2_half1 = fib2 / 10000000000;
+	fib1_half2 = fib1 % 10000000000;
+	fib2_half2 = fib2 % 10000000000;
 
-	for (cnt = 93; cnt < 99; cnt++)
+	for (count = 93; count < 99; count++)
 	{
-		half1 = fib8_half1 + fib9_half1;
-		half2 = fib8_half2 + fib9_half2;
-		if (fib8_half2 + fib9_half2 > 9999999999)
+		half1 = fib1_half1 + fib2_half1;
+		half2 = fib1_half2 + fib2_half2;
+		if (fib1_half2 + fib2_half2 > 9999999999)
 		{
 			half1 += 1;
 			half2 %= 10000000000;
 		}
 
 		printf("%lu%lu", half1, half2);
-		if (cnt != 98)
+		if (count != 98)
 			printf(", ");
 
-		fib8_half1 = fib9_half1;
-		fib8_half2 = fib9_half2;
-		fib9_half1 = half1;
-		fib9_half2 = half2;
+		fib1_half1 = fib2_half1;
+		fib1_half2 = fib2_half2;
+		fib2_half1 = half1;
+		fib2_half2 = half2;
 	}
 	printf("\n");
 	return (0);
