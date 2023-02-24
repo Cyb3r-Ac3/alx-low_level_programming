c823ebda6ac427341e219c6074e14b94b1db6918
Anthony Odey <anthonyogar61@gmail.com>
2023-02-23 17:27:21 -0800

    print line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -7,16 +7,14 @@
  * Return: void
  */
 
+
 void print_line(int n)
 {
-	int x;
+	int i;
 
-	if (n <= 0)
-		_putchar('\n');
-	else
+	for (i = 0; i < n; i++)
 	{
-		for (x = 0; x < n; x++)
-			_putchar(95);
-		_putchar('\n')
+		_putchar('_');
 	}
-}
+	_putchar('\n');
+}}
