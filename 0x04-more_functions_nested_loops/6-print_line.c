6ae94c5ed62ebe3eed2c52dafd31fbe7f5958fd1
Anthony Odey <anthonyogar61@gmail.com>
2023-02-23 23:23:35 -0800

    2 straight lines

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -10,11 +10,14 @@
 
 void print_line(int n)
 {
-	int i;
+	int y;
 
-	for (i = 0; i < n; i++)
+	if (n <= 0)
+		_putchar('\n');
+	else
 	{
-		_putchar('_');
+		for (y = 0; y < n; y++)
+			_putchar(95);
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
