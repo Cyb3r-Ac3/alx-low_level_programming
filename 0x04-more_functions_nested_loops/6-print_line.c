2907b383eb47c832463ce3c83aa41ef1ebe777e3
Anthony Odey <anthonyogar61@gmail.com>
2023-02-23 17:22:12 -0800

    More Numbers

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_line - printing lines
+ * @n: number of times
+ *
+ * Return: void
+ */
+
+void print_line(int n)
+{
+	int x;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (x = 0; x < n; x++)
+			_putchar(95);
+		_putchar('\n')
+	}
+}
