6417def19a23c844543d2d97cff52e854def749d
Anthony Odey <anthonyogar61@gmail.com>
2023-03-01 10:58:58 -0800

    reverse arrays of integers

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverse content of an array of integers
+ * @a: array
+ * @n: integer
+ * Return: void
+ */
+void reverse_array(int *a, int n)
+{
+int k, d;
+
+for (k = 0; (k < (n - 1) / 2); k++)
+	{
+	d = a[k];
+	a[k] = a[n - 1 - k];
+	a[n - 1 - k] = c;
+	}
+}
