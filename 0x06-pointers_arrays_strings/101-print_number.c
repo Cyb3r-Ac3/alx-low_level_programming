18c3d96979f4970260d26dc2d19c5384e7a495d3
Anthony Odey <anthonyogar61@gmail.com>
2023-03-02 17:28:20 -0800

    test run 3

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,16 +8,19 @@
 
 void print_number(int n)
 {
+	unsigned int n1;
+
+	n1 = n;
 	if (n < 0)
 	{
 		_putchar('-');
-		n = -n;
+		n1 = -n;
 	}
 
-	if (n / 10)
+	if (n1 / 10 != 0)
 	{
-		print_number(n / 10);
+		print_number(n1 / 10);
 	}
 
-	_putchar((n % 10) + '0');
+	_putchar((n1 % 10) + '0');
 }
