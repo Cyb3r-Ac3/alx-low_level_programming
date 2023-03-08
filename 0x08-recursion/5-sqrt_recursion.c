287d0548ece3326591d388856beb620e1ec0a1ba
Anthony Odey <anthonyogar61@gmail.com>
2023-03-08 04:51:40 -0800

    test run

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -2,60 +2,29 @@
 #include <stdio.h>
 
 /**
+ * check - checks for the square root
+ * @a:int
+ * @b:int
+ *
+ * Return: int
+ */
+int check(int a, int b)
+{
+	if (a * a == b)
+		return (a);
+	if (a * a > b)
+		return (-1);
+	return (check(a + 1, b));
+}
+
+/**
  * _sqrt_recursion - returns the natural square root of a number
  * @n: integer to find sqrt of
  * Return: natural square root or -1
  */
-
 int _sqrt_recursion(int n)
 {
-	if (n < 0)
-	{
-		/* if n is negative, return1 */
-		return (-1);
-	}
-	else if (n == 0 || n == 1)
-	{
-		/* base cases */
-		return (n);
-	}
-	else
-	{
-		/* recursive case */
-		return (_sqrt_helper(n, 1, n));
-	}
-}
-
-/**
- * _sqrt_helper - that takes three integers n, start, and end
- *                as inputs and returns the natural square root of n
- * @n: integer to find sqrt of
- * @start: calculate the midpoint
- * @end: search rigth half
- * Return: natural square root or -1
- */
-int _sqrt_helper(int n, int start, int end)
-{
-	if (start <= end)
-	{
-		int mid = (start + end) / 2; /* calculate midpoint */
-		int sqr = mid * mid; /* calculate square of midpoint */
-
-		if (sqr == n)
-		{
-			return (mid); /* midpoint is the square root */
-		}
-		else if (sqr > n)
-		{
-			return (_sqrt_helper(n, start, mid - 1)); /* search left half */
-		}
-		else
-		{
-			return (_sqrt_helper(n, mid + 1, end)); /* search right half */
-		}
-	}
-	else
-	{
-		return (-1); /* natural squre root not found */
-	}
+	if (n == 0)
+		return (0);
+	return (check(1, n));
 }
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -4,5 +4,4 @@ void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
-int _sqrt_recursion(int n);
-int _sqrt_helper(int n, int start, int end);
+int _sqrt_recursion(int n);;
