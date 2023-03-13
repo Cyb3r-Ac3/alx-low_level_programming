ad44e9542dcca4fb16133cb2438d49eb9fab5d01
Anthony Odey <anthonyogar61@gmail.com>
2023-03-13 03:01:16 -0700

    Add implementation of create_array function with Betty style

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * create_array - creates an array of chars, and initializes it with
+ *                specific char.
+ * @size: the size of the array
+ * @c: the character to initialize the array with
+ * Return: NULL if size = 0, otherwise a pointer to the array,
+ *         or NULL if it fails
+ */
+char *create_array(unsigned int size, char c)
+{
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
+	char *array = malloc(size * sizeof(char));
+
+	if (array == NULL)
+	{
+		return (NULL);
+	}
+
+	for (unsigned int i = 0; i < size; i++)
+	{
+		array[i] = c;
+	}
+
+	return (array);
+}
---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,7 @@
+#ifndef MAIN-H
+#define MAIN_H
+
+int _putchar(char c);
+char *create_array(unsigned int size, char c);
+
+#endif
