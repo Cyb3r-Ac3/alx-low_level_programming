945d54ec4df0b18749c86a0193a837b1bc685e92
Anthony Odey <anthonyogar61@gmail.com>
2023-03-13 03:21:04 -0700

    Add implementation of create_array function

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,19 +11,21 @@
  */
 char *create_array(unsigned int size, char c)
 {
+	char *array;
+	unsigned int i;
+
 	if (size == 0)
 	{
 		return (NULL);
 	}
 
-	char *array = malloc(size * sizeof(char));
-
+	array = malloc(size * sizeof(char));
 	if (array == NULL)
 	{
 		return (NULL);
 	}
 
-	for (unsigned int i = 0; i < size; i++)
+	for (i = 0; i < size; i++)
 	{
 		array[i] = c;
 	}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,6 +1,5 @@
-#ifndef MAIN-H
+#ifndef MAIN_H
 #define MAIN_H
-
 int _putchar(char c);
 char *create_array(unsigned int size, char c);
 
