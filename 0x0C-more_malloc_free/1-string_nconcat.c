2980e40463bb638244025a7c620fc4af65c1662a
Anthony Odey <anthonyogar61@gmail.com>
2023-03-20 10:35:19 -0700

    wrote a function that concatenates two strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,6 +1,6 @@
-#include "main.h"
 #include <stdlib.h>
-
+#include <stdlib.h>
+#include "main.h"
 /**
  * _strlen - calculate and return string length
  * @string: string
@@ -8,13 +8,12 @@
  */
 int _strlen(char *string)
 {
-	int b;
+	int i;
 
-	for (b = 0; string[b] != '\0'; b++)
+	for (i = 0; string[i] != '\0'; i++)
 		;
-	return (b);
+	return (i);
 }
-
 /**
  * string_nconcat - concatenate s1 and n bytes of s2; return ptr to string
  * @s1: string 1
@@ -22,11 +21,10 @@ int _strlen(char *string)
  * @n: n bytes to concat from string 2
  * Return: pointer to concatenated string
  */
-
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *p;
-	int num, len, a, b;
+	char *ptr;
+	int num, len, i, j;
 
 	num = n;
 
@@ -36,20 +34,20 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s2 = "";
 	if (num < 0) /* account for negative n bytes */
 		return (NULL);
-	if (num >= _strlen(s2)) /* account fo n too big */
-		num = strlen(s2);
+	if (num >= _strlen(s2)) /* account for n too big */
+		num = _strlen(s2);
 
-	len = strlen(s1) + num + 1; /* +1 to account for null pointer */
+	len = _strlen(s1) + num + 1; /* +1 to account for null pointer */
 
-	p = malloc(sizeof(*p) * len); /* malloc and check for error */
-	if (p == NULL)
+	ptr = malloc(sizeof(*ptr) * len); /* malloc and check for error */
+	if (ptr == NULL)
 		return (NULL);
 
-	for (a = 0; s1[a] != '\0'; b++) /* concat */
-		p[a] = s1[a];
-	for (b = 0; b < num; b++)
-		p[a + b] = s2[b];
-	p[a + b] = '\0';
+	for (i = 0; s1[i] != '\0'; i++) /* concat */
+		ptr[i] = s1[i];
+	for (j = 0; j < num; j++)
+		ptr[i + j] = s2[j];
+	ptr[i + j] = '\0';
 
-	return (p);
+	return (ptr);
 }
