d7ef615015f956f51f66ef1e2ebc9b8303bd750c
Anthony Odey <anthonyogar61@gmail.com>
2023-03-20 17:31:46 -0700

    wrote a function that created a new dog

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include "dog.h"
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * new_dog - creates a new dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Return: pointer to the new dog, or NULL if allocation fails
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *newdog = malloc(sizeof(dog_t));
+
+	if (new_dog == NULL)
+		return (NULL);
+
+	new_dog->name = strdup(name);
+	if (new_dog->name == NULL)
+	{
+		free(new_dog);
+		return (NULL);
+	}
+
+	new_dog->owner = strup(owner);
+	if (new_dog->owner == NULL)
+	{
+		free(new_dog->name);
+		free(new_dog);
+		return (NULL);
+	}
+
+	new_dog->age = age;
+
+	return (new_dog);
+}
