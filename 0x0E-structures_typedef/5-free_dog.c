76a43f07a0550f945f0b1db5a3ec84705d0c458c
Anthony Odey <anthonyogar61@gmail.com>
2023-03-20 17:53:28 -0700

    wrote a funtion that frees dogs

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,19 +1,16 @@
-#include <stdio.h>
 #include <stdlib.h>
-#include "main.h"
+#include "dog.h"
 
 /**
- * free_dog - frees the memory allocated for a dog struct
- * @d: pointer to the dog struct to free
+ * free_dog - frees memory allocated for a struct dog
+ * @d: struct dog to free
  */
 void free_dog(dog_t *d)
 {
-	if (d != NULL) /* checke if pointer is not small */
+	if (d)
 	{
-		if (d->name != NUL) /* free name if not NULL */
-			free(d->name);
-		if (d->owner != NULL) /* free owner if NULL */
-			free(d->owner);
-		free(d); /* free the dog struct itself */
+		free(d->name);
+		free(d->owner);
+		free(d);
 	}
 }
