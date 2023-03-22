5e575ee1a3b892fa00fc6d90958510e6c6e45bb0
Anthony Odey <anthonyogar61@gmail.com>
2023-03-22 09:48:18 -0700

    wrote a function that prints a name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,31 +1,14 @@
+#include <stdlib.h>
 #include "function_pointers.h"
-#include <stdio.h>
 
 /**
  * print_name - prints a name
- *
- * @name: pointer to string containing the name to be printed
- * @f: pointer to function that takes a char pointer argument and returns void
- *
- * This function prints the name passed as an argument, using the function
- * pointed to by f to format the output. The format function should take a
- * char pointer argument and return void. If f is NULL, the name is printed
- * without any formatting.
+ * @name: name to print
+ * @f: pointer to the printing function
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (name == NULL)
-	{
+	if (!name || !f)
 		return;
-	}
-
-	if (f != NULL)
-	{
-		f(name);
-	}
-
-	else
-	{
-		printf("%s\n", name);
-	}
+	f(name);
 }
