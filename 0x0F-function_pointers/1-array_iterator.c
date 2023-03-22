3b5108707531ab16f1bd50978bc3f8f32f39c91b
Anthony Odey <anthonyogar61@gmail.com>
2023-03-22 11:46:38 -0700

    wrote a function thaat executes a function given as a parameter on each element of an array

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include "function_pinters.h"
+/**
+ * array_iterator - applies a function to each element of an array
+ * @array: pointer to the array
+ * @size: size of the array
+ * @action: pointer to the function to apply
+ *
+ * This function applies the function pointed to by 'action' to each element
+ * of the array pointed to by 'array', using a loop. The size of the array
+ * is given by 'size'.
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	if (!array || !action)
+	{
+		return;
+	}
+
+	for (size_t i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
