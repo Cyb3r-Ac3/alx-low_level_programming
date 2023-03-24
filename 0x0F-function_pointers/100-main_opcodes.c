cf7681a0f8a1705ce5afb04826625fd4cdcb6913
Anthony Odey <anthonyogar61@gmail.com>
2023-03-24 07:59:03 -0700

    wrote a program that prints the 'opcodes' ofit own main function

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -11,22 +11,33 @@
  */
 int main(int argc, char **argv)
 {
+	int bytes, i;
+	char *arr;
+
 	if (argc != 2)
 	{
 		printf("Error\n");
-		return (1);
+		exit(1);
 	}
 
-	int n_bytes = atoi(argv[1]);
+	bytes = atoi(argv[1]);
 
-	if (n_bytes < 0)
+	if (bytes < 0)
 	{
 		printf("Error\n");
-		return (2);
+		exit(2);
 	}
 
-	unsigned char *start = (unsigned char *) &main;
+	arr = (char *)main;
 
-	print_opcodes(start, n_bytes);
+	for (i = 0; i < bytes; i++)
+	{
+		if (i == bytes - 1)
+		{
+			printf("%02hhx\n", arr[i]);
+			break;
+		}
+		printf("%02hhx ", arr[i]);
+	}
 	return (0);
 }
