bb5ccff2e526edcd1fc097d12314dd30de6e9c50
Anthony Odey <anthonyogar61@gmail.com>
2023-03-24 14:52:32 -0700

    wrote a function that prints numbers, followed by new line

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -10,18 +10,18 @@
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
+	/* creating va_list to store the variable argument list */
 	va_list args;
 	unsigned int i;
-	int num;
 
+	/* init valist for the number of arguments */
 	va_start(args, n);
 
+	/* loop through/access all arguments stored in the valist */
 	for (i = 0; i < n; i++)
 	{
-		num = va_arg(args, int);
-		printf("%d", num);
-
-		if (i < n - 1 && separator != NULL)
+		printf("%d", va_arg(args, unsigned int));
+		if (i < (n - 1) && separator != NULL)
 		{
 			printf("%s", separator);
 		}
