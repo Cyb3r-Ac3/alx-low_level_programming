25e493ab5b2c760356ef323bacd7708b4c7b39e0
Anthony Odey <anthonyogar61@gmail.com>
2023-03-24 16:17:33 -0700

    wrote a function that prints anything

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include <stdlib.h>
-#include "variadic_functions.h"
+
 /**
  * print_all - prints anything
  * @format: list of types of arguments passed to the function
