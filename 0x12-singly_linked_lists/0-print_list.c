49f88e0e57f8a44abe0f62eea2fa9354261e413f
anthonyogar <anthonyogar61@gmail.com>
2023-03-31 19:05:26 -0700

    test run files

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,19 +7,16 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t count;
-
-	count = 0;
+	size_t count = 0;
 
 	while (h != NULL)
 	{
-		if (h->str == NULL || h->len == 0)
-			printf("[%d] %s\n", 0, "(nil)\n");
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
 		else
 			printf("[%d] %s\n", h->len, h->str);
-
-		h = h->next;
 		count++;
+		h = h->next;
 	}
 
 	return (count);
