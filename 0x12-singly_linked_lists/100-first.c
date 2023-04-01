388d0f2ad2d084de203738c52498ff0cada3c70e
anthonyogar <anthonyogar61@gmail.com>
2023-03-31 18:40:23 -0700

    test run file

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -2,12 +2,12 @@
 
 
 /**
- * first - function executed before main
+ * print_turtle_message - Prints a message about a determined turtle
  * Description: This function prints the message "you're beat and yet you
  * must allow,\n i bore my house upon my back!\n", whichis a reference to
  * the story "The tortoise and the hare".
  */
-void __attribute__((constructor)) first()
+void print_turtle_message(void)
 {
 	printf("You're beat! and yet, you  must allow, \n");
 	printf("I bore my house upon my back!\n");
