782b9ec1509c2e1dc53d628fedbcdaaf03381b5a
anthonyogar <anthonyogar61@gmail.com>
2023-03-31 18:03:16 -0700

    wrote a printf function for the tortoise and hare story

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,12 +1,14 @@
 #include <stdio.h.>
 
+void first(void) __attribute__((constructor));
+
 /**
  * first - function executed before main
  * Description: This function prints the message "you're beat and yet you
  * must allow,\n i bore my house upon my back!\n", whichis a reference to
  * the story "The tortoise and the hare".
  */
-void __atribute__((constructor)) first()
+void first(void)
 {
 	printf("You're beat! and yet, you  must allow, \n");
 	printf("I bore my house upon my back!\n");
