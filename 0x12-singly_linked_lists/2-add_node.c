9ab49e9a7ba3d328103652304d485b4f8265a737
anthonyogar <anthonyogar61@gmail.com>
2023-03-31 19:27:36 -0700

    test run 2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -4,27 +4,29 @@
  * @head: a pointer to a pointer to the list_t list
  * @str: the string to be added to the list_t list
  *
- * Return: the address of the new element, or NULL if it failed.
+ * Return: the address of the new element, or NULL if it failed
  */
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node;
+	char *new_str;
 
-	if (str == NULL)
+	if (head == NULL || str == NULL)
 		return (NULL);
 
 	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
 		return (NULL);
 
-	new_node->str = strdup(str);
-	if (new_node->str == NULL)
+	new_str = strdup(str);
+	if (new_str == NULL)
 	{
 		free(new_node);
 		return (NULL);
 	}
 
-	new_node->len = strlen(str);
+	new_node->str = new_str;
+	new_node->len = strlen(new_str);
 	new_node->next = *head;
 	*head = new_node;
 
