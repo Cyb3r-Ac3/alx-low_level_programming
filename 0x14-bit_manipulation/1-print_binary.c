36b296311afe4f7533f004b9568e3ed23f72af9b
anthonyogar <anthonyogar61@gmail.com>
2023-04-08 05:29:33 -0700

    wrote a function that prints the  binary representation of a number

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -5,22 +5,7 @@
  */
 void print_binary(unsigned long int)
 {
-	/* move a 1 bit to the highest position in
-	 * twmporary variable */
-	unsigned long int mask;
-	mask <<= 63;
-
-	mask = 0;
-
-	while ((n & mask) == 0 && mask != 0)
-		mask >>= 1;
-	
-	while (mask != 0)
-	{
-		if ((n & mask) != 0)
-			putchar('1');
-		else
-			putchar('0');
-		mask >>= 1;
-	}
+	if (n > 1)
+		print_binary(n >> 1);
+	_putchar((n & 1) ? '1' : '0');
 }
