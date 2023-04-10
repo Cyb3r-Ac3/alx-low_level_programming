888a95894673570a44887d681cee1f27b4fed9f0
anthonyogar <anthonyogar61@gmail.com>
2023-04-10 12:59:07 -0700

    fixed  the file errors now it can convert a binary num to an unsigned int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * binary_to_unit - converts a binary number to an unsigned
+ * binary_to_uint - converts a binary number to an unsigned
  * int
  * @b: pointer to a string of 0
  * Return: the converted number, or 0 if there is one or
