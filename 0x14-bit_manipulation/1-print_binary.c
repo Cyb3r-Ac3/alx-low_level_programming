27f9e48571c1ea284caa609154b21a0278a4f896
anthonyogar <anthonyogar61@gmail.com>
2023-04-10 13:27:02 -0700

    fixed the bug

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,11 +1,45 @@
 #include "main.h"
 /**
- * print_binary - prints tge binary representation of a number
- * @n: the number to convert
+ * _power - calcualtes base and power
+ * @base: base of the exponet
+ * @pow: power of the exponet
+ *
+ * Return: value of base and power
  */
-void print_binary(unsigned long int)
+unsigned long int _power(unsigned int base, unsigned int pow)
 {
-	if (n > 1)
-		print_binary(n >> 1);
-	_putchar((n & 1) ? '1' : '0');
+	unsigned int k;
+	unsigned long int val = 1;
+
+	for (k = 1; k <= pow; k++)
+		val *= base;
+	return (val);
+}
+
+/**
+ * print_binary - prints the binary representation of a number
+ * @n: num of pretend
+ * Return: void
+ */
+void print_binary(unsigned long int n)
+{
+	char hug = 0;
+	unsigned long int sft, count;
+
+	sft = _power(2, sizeof(unsigned long int) * 8 - 1);
+
+	while (sft != 0)
+	{
+		count = n & sft;
+		if (count == sft)
+		{
+			hug = 1;
+			_putchar('1');
+		}
+		else if (hug == 1 || sft == 1)
+		{
+			_putchar('0');
+		}
+		sft >>= 1;
+	}
 }
