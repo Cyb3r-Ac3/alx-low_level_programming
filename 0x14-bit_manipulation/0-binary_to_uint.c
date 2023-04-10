ef38aac1945fdb42461f0ec7f640e8a47c6e16de
anthonyogar <anthonyogar61@gmail.com>
2023-04-10 12:39:27 -0700

    fixed  the file errors now it can convert a binary num to an unsigned int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -11,16 +11,23 @@
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int val = 0;
-	int i = 0;
+	int i;
 
-	if (b == NULL)
+	if (!b)
 		return (0);
 
-	while (b[i] == '0' || b[i] == '1')
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+	}
+
+	for (i = 0; b[i] != '\0'; i++)
 	{
 		val <<= 1;
-		val += b[i] - '0';
-		i++;
+		if (b[i] == '1')
+			val += 1;
 	}
+
 	return (val);
 }
