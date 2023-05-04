e437388158a18e0457654084bd1de8ea7e751eb1
Anthony ogaiiiiir <anthonyogar61@gmail.com>
2023-05-04 02:48:45 +0100

    wrote a function that adds a new node at th beginning of a listint_t list.

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -11,16 +11,26 @@
 
 listint_t *add_nodeint(listint_t **head, const int n)
 {
-	new_node = malloc(sizeof(listint_t));
-	if (new_node == NULL)
+	listint_t *n_node;
+
+	n_node = malloc(sizeof(listint_t));
+
+	if (n_node != NULL)
+	{
+		n_node->n = new;
+		n_node->next = *head;
+	}
+	
+	else
 	{
 		return (NULL);
 	}
 
-	new_node->n = n;
-	new_node->next = *head;
+	if (*head != NULL)
+	{
+		n_node->next = *head;
+	}
 
 	*head = new_node;
-
 	return (new_node);
 }
