19e12542032522ef18204b0bfc0e340552c6b44e
Anthony ogaiiiiir <anthonyogar61@gmail.com>
2023-05-06 22:35:19 +0100

    syntax error partially fixed

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,25 +1,5 @@
 #include "main.h"
 /**
- * _pow - calculates (base ^ power)
- *
- * @base: base of the exponent
- *
- * @power: power of the exponent
- *
- * Return: value of (base ^ power)
- */
-unsigned long int _pow(unsigned int base, unsigned int power)
-{
-	unsigned long int result = 1;
-	unsigned int k;
-
-	for (k = 0; k < power; k++)
-		result *= base;
-	return (result);
-}
-
-
-/**
  * print_binary - prints the binary representation of a number
  *
  * @n: the number to print
@@ -28,23 +8,21 @@ unsigned long int _pow(unsigned int base, unsigned int power)
  */
 void print_binary(unsigned long int n)
 {
-	unsigned long int num;
-	int print = 0;
+	int i, count = 0;
+	unsigned long int current;
 
-
-	num = _pow(2, sizeof(unsigned long int) * 8 - 1);
-
-	while (num != 0)
+	for (i = 63; i >= 0; i--)
 	{
-		if (n & num)
+		current = n >> i;
+
+		if (current & 1)
 		{
-			putchar('1');
-			print = 1;
+			_putchar('1');
+			count++;
 		}
-		else if (print)
-			putchar('0');
-		num >>= 1;
+		else if (count)
+			_putchar('0');
 	}
-	if (!print)
-		putchar('0);
+	if (!count)
+		_putchar('0');
 }
